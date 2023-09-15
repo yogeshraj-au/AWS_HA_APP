9aa6ff64c0a9b821ea1d445fdedc7ae4419ebb0a
yogeshraj28 <34445437+yogeshraj-au@users.noreply.github.com>
2023-09-15 15:29:39 +0530

    add files

---
+++ modules/amilookup/main.tf
@@ -0,0 +1,16 @@
+#Define the data block to retrieve the latest ami-id
+data "aws_ami" "ubuntu_server" {
+  most_recent = true
+  owners      = ["099720109477"]
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
---
+++ providers.tf
@@ -0,0 +1,13 @@
+#Define the cloud provider, version and region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = var.region
+}
